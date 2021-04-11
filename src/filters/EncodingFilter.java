ba8c907545e96164d138f42c5437cc9c2bc498a6
miyazawa <56.-.ri.-.56@au.com>
2021-04-11 12:44:39 +0900

    Modify Index/NewServlet

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -20,6 +20,9 @@ public class EncodingFilter implements Filter {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
         chain.doFilter(request, response);
     }
 
